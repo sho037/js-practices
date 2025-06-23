02b644fd084b847d3409c800ad15190efdcc9bef
sho037 <103867061+sho037@users.noreply.github.com>
2025-06-23 10:30:57 +0900

    refactor: 余分な空行やインデントが入らないように

--- 03.asynchronous/query.js
+++ 03.asynchronous/query.js
@@ -1,32 +1,18 @@
 // ===ここから正常系===
-export const CREATE_BOOKS_TABLE_QUERY = `
-  CREATE TABLE IF NOT EXISTS books (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL UNIQUE
-  )
-`;
+export const CREATE_BOOKS_TABLE_QUERY =
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
 
-export const INSERT_BOOK_QUERY = `
-  INSERT INTO books (title)
-  VALUES (?)
-`;
+export const INSERT_BOOK_QUERY = "INSERT INTO books (title) VALUES (?)";
 
-export const SELECT_BOOK_QUERY = `
-  SELECT * FROM books WHERE id = (?)
-`;
+export const SELECT_BOOK_QUERY = "SELECT * FROM books WHERE id = (?)";
 
-export const DROP_BOOKS_TABLE_QUERY = `
-  DROP TABLE books
-`;
+export const DROP_BOOKS_TABLE_QUERY = "DROP TABLE books";
 // ===ここまで正常系===
 
 // ===ここから異常系===
-export const ERROR_INSERT_BOOK_QUERY = `
-  INSERT INTO books (not_exist_column)
-  VALUES (?)
-`;
+export const ERROR_INSERT_BOOK_QUERY =
+  "INSERT INTO books (not_exist_column) VALUES (?)";
 
-export const ERROR_SELECT_BOOK_QUERY = `
-  SELECT * FROM not_exist_table WHERE id = (?)
-`;
+export const ERROR_SELECT_BOOK_QUERY =
+  "SELECT * FROM not_exist_table WHERE id = (?)";
 // ===ここまで異常系===
